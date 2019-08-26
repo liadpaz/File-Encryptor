bdf0188caca02a23ef87c04a1169629597cacca2
liadpaz <liadpazhamud@gmail.com>
2019-08-26 13:40:19 +0300

    ""

---
+++ FileEncryptor/FileEncryptor/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace FileEncryptor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Password = new System.Windows.Forms.TextBox();
+            this.State = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.FileChooser = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Path_Box = new System.Windows.Forms.TextBox();
+            this.Cryptor = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(129, 116);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(217, 20);
+            this.Password.TabIndex = 0;
+            // 
+            // State
+            // 
+            this.State.BackColor = System.Drawing.Color.Lime;
+            this.State.Enabled = false;
+            this.State.Font = new System.Drawing.Font("Arial", 9F);
+            this.State.Location = new System.Drawing.Point(350, 49);
+            this.State.Name = "State";
+            this.State.Size = new System.Drawing.Size(50, 50);
+            this.State.TabIndex = 1;
+            this.State.Text = "Status";
+            this.State.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(2, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter password:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // FileChooser
+            // 
+            this.FileChooser.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.FileChooser.Location = new System.Drawing.Point(29, 63);
+            this.FileChooser.Name = "FileChooser";
+            this.FileChooser.Size = new System.Drawing.Size(94, 22);
+            this.FileChooser.TabIndex = 4;
+            this.FileChooser.Text = "Choose File...";
+            this.FileChooser.UseVisualStyleBackColor = true;
+            this.FileChooser.Click += new System.EventHandler(this.FileChooser_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(372, 36);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Choose file to encrypt/decrypt. Press the \"Action\" button\nwhen GREEN to encrypt, press when RED to decrypt";
+            // 
+            // Path_Box
+            // 
+            this.Path_Box.Location = new System.Drawing.Point(129, 65);
+            this.Path_Box.Name = "Path_Box";
+            this.Path_Box.ReadOnly = true;
+            this.Path_Box.Size = new System.Drawing.Size(217, 20);
+            this.Path_Box.TabIndex = 6;
+            // 
+            // Cryptor
+            // 
+            this.Cryptor.BackColor = System.Drawing.Color.Gray;
+            this.Cryptor.Font = new System.Drawing.Font("Arial", 9F);
+            this.Cryptor.Location = new System.Drawing.Point(350, 100);
+            this.Cryptor.Name = "Cryptor";
+            this.Cryptor.Size = new System.Drawing.Size(50, 50);
+            this.Cryptor.TabIndex = 1;
+            this.Cryptor.Text = "Action";
+            this.Cryptor.UseVisualStyleBackColor = false;
+            this.Cryptor.Click += new System.EventHandler(this.Cryptor_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Arial", 10F);
+            this.checkBox1.Location = new System.Drawing.Point(129, 142);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(125, 20);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Show password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 161);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Path_Box);
+            this.Controls.Add(this.FileChooser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Cryptor);
+            this.Controls.Add(this.State);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "File Cryptor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Button State;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button FileChooser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Path_Box;
+        private System.Windows.Forms.Button Cryptor;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ FileEncryptor/FileEncryptor/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+
+namespace FileEncryptor
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = null;
+
+        [DllImport("KERNEL32.DLL", EntryPoint = "RtlZeroMemory")]
+        public static extern bool ZeroMemory(IntPtr Destination, int Length);
+
+        public Form1()
+        {
+            InitializeComponent();
+            Cryptor.Enabled = false;
+            Password.UseSystemPasswordChar = true;
+            State.BackColor = Color.Gray;
+        }
+
+        private void Cryptor_Click(object sender, EventArgs e)
+        {
+            if (Password.Text != null)
+            {
+                string password = Password.Text;
+                if (State.BackColor == Color.Green)
+                {
+                    if (CheckPassword(password))
+                    {
+                        GCHandle gch = GCHandle.Alloc(password, GCHandleType.Pinned);
+                        FileEncrypt(filePath, password);
+                        File.Delete(filePath);
+                        ZeroMemory(gch.AddrOfPinnedObject(), password.Length * 2);
+                        gch.Free();
+                        CheckFile();
+                        MessageBox.Show("Successfully encryped the file!");
+                    }
+                }
+                else if (State.BackColor == Color.Red)
+                {
+                    GCHandle gch = GCHandle.Alloc(password, GCHandleType.Pinned);
+                    try
+                    {
+                        FileDecrypt(filePath, TrimAES(filePath), password);
+                        File.Delete(filePath);
+                        CheckFile();
+                        MessageBox.Show("Successfully decrypted the file!");
+                    }
+                    catch (Exception error)
+                    {
+                        CheckFile();
+                        MessageBox.Show(error.Message);
+                    }
+                    finally
+                    {
+                        ZeroMemory(gch.AddrOfPinnedObject(), password.Length * 2);
+                        gch.Free();
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Enter password...");
+        }
+
+        private void FileChooser_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                CheckFile();
+                Path_Box.Text = filePath;
+                Cryptor.Enabled = true;
+            }
+        }
+
+        /// <summary>Trims the ".aes" at the end of the string</summary>
+        /// <param name="str">A string with ".aes" at the end</param>
+        private string TrimAES(string str)
+        {
+            return str.Remove(str.Length - 4, 4);
+        }
+
+        private void CheckFile()
+        {
+            if (File.Exists(filePath))
+            {
+                if (filePath.EndsWith(".aes"))
+                    State.BackColor = Color.Red;
+                else
+                    State.BackColor = Color.Green;
+            }
+            else
+            {
+                if (File.Exists(filePath + ".aes"))
+                {
+                    filePath += ".aes";
+                    State.BackColor = Color.Red;
+                }
+                else if (File.Exists(TrimAES(filePath)))
+                {
+                    filePath = TrimAES(filePath);
+                    State.BackColor = Color.Green;
+                }
+                else
+                {
+                    filePath = null;
+                    State.BackColor = Color.Gray;
+                    State.Enabled = false;
+                    MessageBox.Show("File does not exist!\nChoose an existing file.");
+                }
+            }
+        }
+
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    rng.GetBytes(data);
+                }
+            }
+            return data;
+        }
+
+        private void FileEncrypt(string inputFile, string password)
+        {
+            byte[] salt = GenerateRandomSalt();
+
+            FileStream fsCrypt = new FileStream(inputFile + ".aes", FileMode.Create);
+
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            AES.Mode = CipherMode.CFB;
+
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                Application.DoEvents();
+                cs.Write(buffer, 0, read);
+            }
+
+            fsIn.Close();
+
+            cs.Close();
+            fsCrypt.Close();
+        }
+
+        private void FileDecrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CFB;
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents();
+                    fsOut.Write(buffer, 0, read);
+                }
+
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+            catch
+            {
+                fsOut.Close();
+                fsCrypt.Close();
+                File.Delete(outputFile);
+                throw new Exception("Wrong password!");
+            }
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.CheckState == CheckState.Checked)
+                Password.UseSystemPasswordChar = false;
+            else
+                Password.UseSystemPasswordChar = true;
+        }
+
+        private bool CheckPassword(string password)
+        {
+            bool UpperLegal = false, LengthLegal = false, NumberLegal = false;
+            if (password.Length > 6)
+            {
+                LengthLegal = true;
+            }
+            foreach (char x in Password.Text)
+            {
+                if (x.ToString() == x.ToString().ToUpper())
+                    UpperLegal = true;
+                if (int.TryParse(x.ToString(), out int y))
+                    NumberLegal = true;
+            }
+            if (UpperLegal && LengthLegal && NumberLegal)
+                return true;
+            if (MessageBox.Show("Password is weak! Are you sure you want this password?", "Weak password", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                return true;
+            return false;
+        }
+    }
+}
